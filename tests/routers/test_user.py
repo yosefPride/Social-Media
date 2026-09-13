90bda218f6daf371c368eb6dde68b29d1c4c9cc0
yosef <yosefchaimpride@gmail.com>
2026-09-13 20:33:44 +0300

    Add login tests

--- tests/routers/test_user.py
+++ tests/routers/test_user.py
@@ -24,3 +24,23 @@ async def test_register_user_already_exists(
     )
     assert response.status_code == 400
     assert "already exists" in response.json()["detail"]
+
+
+@pytest.mark.anyio
+async def test_login_user_non_existant(async_client: AsyncClient):
+    response = await async_client.post(
+        "/login", json={"email": "test@example.net", "password": "1234"}
+    )
+    assert response.status_code == 401
+
+
+@pytest.mark.anyio
+async def test_login(async_client: AsyncClient, registered_user: dict):
+    response = await async_client.post(
+        "/login",
+        json={
+            "email": registered_user["email"],
+            "password": registered_user["password"],
+        },
+    )
+    assert response.status_code == 200
