cf96c701c86f48eb33ba7dea6550b4d74fd6c85b
yosef <yosefchaimpride@gmail.com>
2026-09-15 11:59:36 +0300

    Reject non-confirmed users

--- app/security.py
+++ app/security.py
@@ -105,6 +105,8 @@ async def authenticate_user(email: str, password: str):
         raise create_credentials_exception("Invalid email or password")
     if not verify_password(password, user.password):
         raise create_credentials_exception("Invalid email or password")
+    if not user.confirmed:
+        raise create_credentials_exception("User has not confirmed email")
     return user
 
 
