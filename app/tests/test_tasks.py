e78485f23ef099f6131dced4a5a316c8daca2b46
yosef <yosefchaimpride@gmail.com>
2026-09-16 11:31:16 +0300

    Add tests for mailgun task

---
+++ app/tests/test_tasks.py
@@ -0,0 +1,19 @@
+import httpx
+import pytest
+
+from app.tasks import APIResponseError, send_simple_email
+
+
+@pytest.mark.anyio
+async def test_send_simple_email(mock_httpx_client):
+    await send_simple_email("test@example.net", "Test subject", "Test body")
+    mock_httpx_client.post.assert_called()
+
+
+@pytest.mark.anyio
+async def test_send_simple_email_api_error(mock_httpx_client):
+    mock_httpx_client.post.return_value = httpx.Response(
+        status_code=500, content="", request=httpx.Request("POST", "//")
+    )
+    with pytest.raises(APIResponseError):
+        await send_simple_email("test@example.net", "Test subject", "Test body")
