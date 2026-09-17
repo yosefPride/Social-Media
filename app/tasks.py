d04b37606be85354ada4c83a36c2707cac4227c5
yosef <yosefchaimpride@gmail.com>
2026-09-17 16:37:54 +0300

    feat: add image generation for posts

--- app/tasks.py
+++ app/tasks.py
@@ -1,8 +1,11 @@
 import logging
+from json import JSONDecodeError
 
 import httpx
+from databases import Database
 
 from app.config import config
+from app.database import post_table
 
 logger = logging.getLogger(__name__)
 
@@ -46,3 +49,69 @@ async def send_user_registration_email(email: str, confirmation_url: str):
             f" following link: {confirmation_url}"
         ),
     )
+
+
+async def _generate_cute_creature_api(prompt: str):
+    logger.debug("Generating cute creature")
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                "https://api.deepai.org/api/cute-creature-generator",
+                data={"text": prompt},
+                headers={"api-key": config.DEEPAI_API_KEY},
+                timeout=60,
+            )
+            logger.debug(response)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as err:
+            raise APIResponseError(
+                f"API request failed with status code {err.response.status_code}"
+            ) from err
+        except (JSONDecodeError, TypeError) as err:
+            raise APIResponseError("API response parsing failed") from err
+
+
+async def generate_and_add_to_post(
+    email: str,
+    post_id: int,
+    post_url: str,
+    database: Database,
+    prompt: str = "A blue british shorthair cat is sitting on a couch",
+):
+    try:
+        response = await _generate_cute_creature_api(prompt)
+    except APIResponseError:
+        return await send_simple_email(
+            email,
+            "Error generating image",
+            (
+                f"Hi {email}! Unfortunately there was an error generating an image"
+                " for your post."
+            ),
+        )
+
+    logger.debug("connecting to database to update post")
+
+    query = (
+        post_table.update()
+        .where(post_table.c.id == post_id)
+        .values(image_url=response["output_url"])
+    )
+
+    logger.debug(query)
+
+    await database.execute(query)
+
+    logger.debug("Database connection in background task closed")
+
+    await send_simple_email(
+        email,
+        "Image generation completed",
+        (
+            f"Hi {email}! Your image has been generated and added to your post."
+            f"Please click on the following link to view it: {post_url}"
+        ),
+    )
+
+    return response
