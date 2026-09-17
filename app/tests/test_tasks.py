fe155f619a6a254a141dfa2983f6635e559380cf
yosef <yosefchaimpride@gmail.com>
2026-09-17 23:54:06 +0300

    test: add image generation tests

--- app/tests/test_tasks.py
+++ app/tests/test_tasks.py
@@ -1,7 +1,14 @@
 import httpx
 import pytest
+from databases import Database
 
-from app.tasks import APIResponseError, send_simple_email
+from app.database import post_table
+from app.tasks import (
+    APIResponseError,
+    _generate_cute_creature_api,
+    generate_and_add_to_post,
+    send_simple_email,
+)
 
 
 @pytest.mark.anyio
@@ -17,3 +24,63 @@ async def test_send_simple_email_api_error(mock_httpx_client):
     )
     with pytest.raises(APIResponseError):
         await send_simple_email("test@example.net", "Test subject", "Test body")
+
+
+@pytest.mark.anyio
+async def test_generate_cute_creture_api_success(mock_httpx_client):
+    json_data = {"output_url": "https://example.com/image.jpg"}
+
+    mock_httpx_client.post.return_value = httpx.Response(
+        status_code=200, json=json_data, request=httpx.Request("POST", "//")
+    )
+
+    result = await _generate_cute_creature_api("A cat")
+
+    assert result == json_data
+
+
+@pytest.mark.anyio
+async def test_generate_cute_creture_api_server_error(mock_httpx_client):
+    mock_httpx_client.post.return_value = httpx.Response(
+        status_code=500, content="", request=httpx.Request("POST", "//")
+    )
+
+    with pytest.raises(
+        APIResponseError, match="API request failed with status code 500"
+    ):
+        await _generate_cute_creature_api("A cat")
+
+
+@pytest.mark.anyio
+async def test_generate_cute_creture_api_json_error(mock_httpx_client):
+    mock_httpx_client.post.return_value = httpx.Response(
+        status_code=200, content="not JSON", request=httpx.Request("POST", "//")
+    )
+
+    with pytest.raises(APIResponseError, match="API response parsing failed"):
+        await _generate_cute_creature_api("A cat")
+
+
+@pytest.mark.anyio
+async def test_generate_and_add_to_post_success(
+    mock_httpx_client, created_post: dict, confirmed_user: dict, db: Database
+):
+    json_data = {"output_url": "https://example.com/image.jpg"}
+
+    mock_httpx_client.post.return_value = httpx.Response(
+        status_code=200, json=json_data, request=httpx.Request("POST", "//")
+    )
+
+    await generate_and_add_to_post(
+        confirmed_user["email"],
+        created_post["id"],
+        "posts/1",
+        db,
+        "A cat",
+    )
+
+    query = post_table.select().where(post_table.c.id == created_post["id"])
+
+    updated_post = await db.fetch_one(query)
+
+    assert updated_post["image_url"] == json_data["output_url"]
