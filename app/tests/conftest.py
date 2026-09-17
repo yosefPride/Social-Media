837386a162f42edad5301d8c3741bb126472f574
yosef <yosefchaimpride@gmail.com>
2026-09-17 16:13:40 +0300

    chore: fix register endpoint spelling

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -43,7 +43,7 @@ async def async_client(client) -> AsyncGenerator:
 @pytest.fixture()
 async def registered_user(async_client: AsyncClient) -> dict:
     user_details = {"email": "test@example.net", "password": "1234"}
-    await async_client.post("register", json=user_details)
+    await async_client.post("/register", json=user_details)
     query = user_table.select().where(user_table.c.email == user_details["email"])
     user = await database.fetch_one(query)
     user_details["id"] = user.id
