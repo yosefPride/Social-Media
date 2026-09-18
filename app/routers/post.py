517bb20c12af7a4c94199ea5585dcab71fba6a3b
yosef <yosefchaimpride@gmail.com>
2026-09-18 19:48:11 +0300

    feat: add prompt for image generation for posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -3,7 +3,7 @@ from enum import Enum
 from typing import Annotated
 
 import sqlalchemy
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, Request
 
 from app.database import comment_table, database, like_table, post_table
 from app.models.post import (
@@ -18,6 +18,7 @@ from app.models.post import (
 )
 from app.models.user import User
 from app.security import get_current_user
+from app.tasks import generate_and_add_to_post
 
 router = APIRouter()
 
@@ -41,7 +42,11 @@ async def find_post_by_id(post_id: int):
 
 @router.post("/posts", response_model=UserPost, status_code=201)
 async def create_post(
-    post: UserPostIn, current_user: Annotated[User, Depends(get_current_user)]
+    post: UserPostIn,
+    current_user: Annotated[User, Depends(get_current_user)],
+    background_tasks: BackgroundTasks,
+    request: Request,
+    prompt: str = None,
 ):
     data = {**post.model_dump(), "user_id": current_user.id}
     query = post_table.insert().values(data)
@@ -50,6 +55,17 @@ async def create_post(
     logger.debug(query)
 
     last_record_id = await database.execute(query)
+
+    if prompt:
+        background_tasks.add_task(
+            generate_and_add_to_post,
+            current_user.email,
+            last_record_id,
+            request.url_for("get_post_with_comments", post_id=last_record_id),
+            database,
+            prompt,
+        )
+
     return {**data, "id": last_record_id}
 
 
--- app/tests/routers/test_post.py
+++ app/tests/routers/test_post.py
@@ -6,6 +6,14 @@ from app.tests.helpers import create_comment, create_post, like_post
 
 
 @pytest.fixture()
+def mock_generate_cute_creature_api(mocker):
+    return mocker.patch(
+        "app.tasks._generate_cute_creature_api",
+        return_value={"output_url": "https://example.net/image.jpg"},
+    )
+
+
+@pytest.fixture()
 async def created_comment(
     async_client: AsyncClient, created_post: dict, logged_in_token: str
 ):
@@ -33,6 +41,27 @@ async def test_create_post(
         "user_id": confirmed_user["id"],
         "image_url": None,
     }.items() <= response.json().items()
+
+
+@pytest.mark.anyio
+async def test_create_post_with_prompt(
+    async_client: AsyncClient, logged_in_token: str, mock_generate_cute_creature_api
+):
+    body = "Test post"
+
+    response = await async_client.post(
+        "/posts?prompt=A cat",
+        json={"body": body},
+        headers={"Authorization": f"Bearer {logged_in_token}"},
+    )
+
+    assert response.status_code == 201
+    assert {
+        "id": 1,
+        "body": body,
+        "image_url": None,
+    }.items() <= response.json().items()
+    mock_generate_cute_creature_api.assert_called()
 
 
 @pytest.mark.anyio
